f5ca9714135b544eb945ffb91bdcf7f2f6fa32ff
Dmitriy Miroshnichenko <d.mir.posts@gmail.com>
2022-01-17 22:46:55 +0300

    add files comparison logic

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
+import path from 'path';
+import { cwd } from 'process';
+import { readFileSync } from 'fs';
+import getDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -7,6 +11,12 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]');
+  .option('-f, --format [type]')
+  .action((filepath1, filepath2, options, command) => {
+    const file1 = JSON.parse(readFileSync(path.resolve(cwd(), filepath1), 'utf-8'));
+    const file2 = JSON.parse(readFileSync(path.resolve(cwd(), filepath2), 'utf-8'));
+
+    getDiff(file1, file2, options.format);
+  });
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
-const genDiff = () => {
-  const object = {
-    name: 'Dmitriy',
+import _ from 'lodash';
+
+const genDiff = (file1, file2, format) => {
+  const keys1 = Object.keys(file1);
+  const keys2 = Object.keys(file2);
+  const keys = _.sortBy(_.union([...keys1, ...keys2]));
+
+  const iter = (currentValue, depth) => {
+    if (!_.isArray(currentValue)) {
+      return currentValue;
+    }
+
+    const indent = ' '.repeat(depth * 2);
+
+    const lines = keys.map((key) => {
+      const value1 = file1[key];
+      const value2 = file2[key];
+
+      if (typeof file1[key] === 'undefined') {
+        return `${indent}+ ${key}: ${value2}`;
+      }
+      if (typeof file2[key] === 'undefined') {
+        return `${indent}- ${key}: ${value1}`;
+      }
+      if (file1[key] !== file2[key]) {
+        return `${indent}- ${key}: ${value1}\n${indent}+ ${key}: ${value2}`;
+      }
+
+      return `${indent}  ${key}: ${value1}`;
+    });
+
+    return ['{', ...lines, '}'].join('\n');
   };
 
-  return object;
+  const result = iter(keys, 1);
+  console.log(result);
+  return result;
 };
 
 export default genDiff;
