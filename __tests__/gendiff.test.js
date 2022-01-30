f2f69ceac9f2c313df89d288ea1ce988c6661d08
Dmitriy Miroshnichenko <d.mir.posts@gmail.com>
2022-01-30 21:02:26 +0300

    remove let variables from tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,19 +7,10 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-let resultStylish;
-let resultPlain;
-let resultJson;
-
-beforeAll(() => {
-  resultStylish = readFile('result-stylish.txt');
-  resultPlain = readFile('result-plain.txt');
-  resultJson = readFile('result-json.json');
-});
-
 test('gendiff stylish format', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.yml');
+  const resultStylish = readFile('result-stylish.txt');
   expect(genDiff(filepath1, filepath2)).toEqual(resultStylish);
   expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(resultStylish);
 });
@@ -27,11 +18,13 @@ test('gendiff stylish format', () => {
 test('gendiff plain format', () => {
   const filepath1 = getFixturePath('file1.yml');
   const filepath2 = getFixturePath('file2.json');
+  const resultPlain = readFile('result-plain.txt');
   expect(genDiff(filepath1, filepath2, 'plain')).toEqual(resultPlain);
 });
 
 test('gendiff json format', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
+  const resultJson = readFile('result-json.json');
   expect(genDiff(filepath1, filepath2, 'json')).toEqual(resultJson);
 });
